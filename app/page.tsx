020e2a73fbd6bc41eee2e9ab73f439455ea7f101
Logan Olson <loganolsonn@gmail.com>
2024-01-10 14:09:30 -0600

    Setup map loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
-import { useLoadScript } from '@react-google-maps/api';
+'use client';
+
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import { useMemo } from 'react';
 
 export default function Home() {
-  const libraries = useMemo(() => ['places'], []);
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-    libraries: libraries as any,
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
   });
+  const mapCenter = { lat: 27.672932021393862, lng: 85.31184012689732 };
+  const mapOptions = {
+    disableDefaultUI: true,
+    clickableIcons: true,
+    scrollwheel: false,
+  };
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <div>Map Script Loaded...</div>;
+  return (
+    <>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={{ height: '100vh', width: '100%' }}
+          center={mapCenter}
+          zoom={15}
+          options={mapOptions}
+        />
+      )}
+    </>
+  );
 }
